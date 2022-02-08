be60d4201a0c677c2bc3a8df342a45b5e33fec0f
Chaitanya Kulkarni <chaitanykulkarni21@gmail.com>
2022-02-08 12:53:59 +0530

    Added Ham

--- api/index.js
+++ api/index.js
@@ -44,6 +44,35 @@ app.get("/api/sign", (req, res) => {
     res.status(200).json({ symbols, sigml });
 });
 
+function auth(req, res, next) {
+    const auth = { login: "admin", password: "password" }; // change this
+
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+    const [login, password] = Buffer.from(b64auth, "base64")
+        .toString()
+        .split(":");
+
+    // Verify login and password are set and correct
+    if (
+        login &&
+        password &&
+        login === auth.login &&
+        password === auth.password
+    ) {
+        // Access granted...
+        return next();
+    }
+
+    // Access denied...
+    res.set("WWW-Authenticate", 'Basic realm="401"'); // change this
+    res.status(401).send("Authentication required."); // custom message
+}
+
+app.get("/ham", auth, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../public/hamnosys.html"));
+});
+
 // app.listen(PORT, () => {
 //     console.log(`Listening on port ${PORT}`);
 // });
