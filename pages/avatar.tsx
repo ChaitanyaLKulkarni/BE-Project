342b3087f457ac1c0f9526ab704db52dc62e08af
Chaitanya Kulkarni <chaitanykulkarni21@gmail.com>
2022-03-10 20:44:13 +0530

    Avatar page style

--- pages/avatar.tsx
+++ pages/avatar.tsx
@@ -28,46 +28,39 @@ const AvatarPage: NextPage = ({}: Props) => {
             <NavBar />
             <div className={styles.container}>
                 <div className={styles.controlls}>
-                    <p className={styles.heading}>Text to ISL Converter</p>
+                    {/* <p className={styles.heading}>Text to ISL Converter</p>
                     <label className={styles.label} htmlFor="menu">
                         Avatar: &emsp;
                     </label>
                     <span className="CWASAAvMenu av0" id="menu"></span>
-                    <br />
-                    <br />
-                    <br />
                     <label className={styles.label} htmlFor="textInp">
                         Enter the Text: &emsp;
-                    </label>
+                    </label> */}
                     <textarea
                         className={styles.textarea}
-                        cols={30}
-                        rows={5}
-                        placeholder="Enter text"
+                        placeholder="Enter English text here"
                         value={inpText}
                         onChange={(e) => setInpText(e.target.value)}
                     ></textarea>
-                    <br />
-                    <br />
-                    <br />
-                    <button
-                        className={`${styles.btn} ${styles.play}`}
-                        disabled={isPlaying || isLoading}
-                        onClick={() => requestAndPlaySiGML(inpText)}
-                    >
-                        Play
-                    </button>
-                    &emsp;
-                    <button
-                        className={`${styles.btn} ${styles.stop}`}
-                        disabled={!isPlaying || isLoading}
-                        onClick={stopPlaying}
-                    >
-                        Stop
-                    </button>
-                    <br />
-                    <br />
+                    <div className={styles.btnsContainer}>
+                        <button
+                            className={`${styles.btn} ${styles.play}`}
+                            disabled={isPlaying || isLoading}
+                            onClick={() => requestAndPlaySiGML(inpText)}
+                        >
+                            Play
+                        </button>
+                        &emsp;
+                        <button
+                            className={`${styles.btn} ${styles.stop}`}
+                            disabled={!isPlaying || isLoading}
+                            onClick={stopPlaying}
+                        >
+                            Pause
+                        </button>
+                    </div>
                 </div>
+                <div style={{ flex: 0.5 }} /> {/* spacer */}
                 <div className={styles.avatarContainer}>
                     <div className="CWASAAvatar av0"></div>
                     <div className={styles.textContainer}>
