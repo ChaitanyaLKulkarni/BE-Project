e1054777a1f5bb2981d406effc605b857808ee4e
Chaitanya Kulkarni <chaitanykulkarni21@gmail.com>
2022-03-13 17:18:13 +0530

    Client Lesson Complete

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -20,25 +20,18 @@ const LessonsPage = (props: Props) => {
         }
     }, [allChapters]);
 
-    useEffect(() => {
-        if (chapter) {
-            setLesson(chapter.lessons[0]);
-        }
-    }, [chapter]);
-
-    const handleChapterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleChapterChange = (c: IChapter) => {
         setLesson(undefined);
-        const selectedChapter = allChapters?.find(
-            (l) => l.title === e.target.value
-        );
-        setChapter(selectedChapter);
+        if (c._id === chapter?._id) setChapter(undefined);
+        else setChapter(c);
     };
 
-    const handleLessonChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const lesson = chapter?.lessons?.find(
-            (l) => l.symbol === e.target.value
-        );
-        setLesson(lesson);
+    const handleLessonChange = (l: ILesson) => {
+        if (l._id === lesson?._id) setLesson(undefined);
+        else {
+            setLesson(l);
+            CWASA?.playSiGMLText(l.sigml);
+        }
     };
 
     return (
@@ -46,93 +39,47 @@ const LessonsPage = (props: Props) => {
             <NavBar />
             <div className={styles.container}>
                 <div className={styles.lessonsContainer}>
-                    <select
-                        onChange={handleChapterChange}
-                        value={chapter?.title}
-                    >
-                        {allChapters?.map((ch) => (
-                            <option key={ch._id?.toString()} value={ch.title}>
-                                {ch.title}
-                            </option>
-                        ))}
-                    </select>
-                    {chapter && (
-                        <select
-                            onChange={handleLessonChange}
-                            value={lesson?.symbol}
-                        >
-                            {chapter?.lessons.map((l) => (
-                                <option
-                                    key={l._id?.toString()}
-                                    value={l.symbol}
-                                >
-                                    {l.symbol}
-                                </option>
-                            ))}
-                        </select>
-                    )}
-
-                    {/* Chapter Info */}
-                    <div className={styles.createChapter}>
-                        <h2>Chapter Info</h2>
-                        <br />
-                        <div>
-                            Title :{" "}
-                            <span className={styles.infoText}>
-                                {chapter?.title}
-                            </span>
-                        </div>
-                        <br />
-                        <div>
-                            Description :{" "}
-                            <span className={styles.infoText}>
-                                {chapter?.description}
-                            </span>
-                        </div>
-                        <br />
-                        <div>
-                            Difficulty :{" "}
-                            <span className={styles.infoText}>
-                                {chapter?.difficulty}
-                            </span>
-                        </div>
-                        <br />
-                        <div>
-                            Lessons :{" "}
-                            <span className={styles.infoText}>
-                                {chapter?.lessons.length}
-                            </span>
-                        </div>
-                        <br />
-                    </div>
-                    {/* Lesson Info */}
-                    {lesson && (
-                        <div className={styles.createChapter}>
-                            <h2>Lesson Info</h2>
-                            <br />
-                            <div>
-                                Symbol :{" "}
-                                <span className={styles.infoText}>
-                                    {lesson?.symbol}
-                                </span>
-                            </div>
-                            <br />
-                            <div>
-                                Description :{" "}
-                                <span className={styles.infoText}>
-                                    {lesson?.description}
-                                </span>
-                            </div>
-                            <br />
-                            <button
-                                onClick={() =>
-                                    CWASA?.playSiGMLText(lesson?.sigml ?? "")
-                                }
+                    {allChapters.map((c) => (
+                        <div key={c.title} className={styles.chapterContainer}>
+                            <div
+                                className={`${styles.chapterInfo} ${
+                                    chapter?._id === c._id
+                                        ? styles.selected
+                                        : ""
+                                }`}
+                                onClick={() => handleChapterChange(c)}
+                                title={c.description}
                             >
-                                Play
-                            </button>
+                                <span>
+                                    Chapter: {c.title}
+                                    <br />
+                                    <span className={styles.smallText}>
+                                        {c.description}
+                                    </span>
+                                </span>
+                            </div>
+                            {chapter?.title === c.title && (
+                                <div className={styles.chapterLessons}>
+                                    {c.lessons.map((l) => (
+                                        <div
+                                            key={l.symbol}
+                                            className={`${styles.lessonInfo} ${
+                                                lesson?._id === l._id
+                                                    ? styles.LessonSelected
+                                                    : ""
+                                            }`}
+                                            onClick={() =>
+                                                handleLessonChange(l)
+                                            }
+                                            title={l.description}
+                                        >
+                                            {l.symbol}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                    )}
+                    ))}
                 </div>
                 <div className={styles.avatarContainer}>
                     <div className="CWASAAvatar av0"></div>
